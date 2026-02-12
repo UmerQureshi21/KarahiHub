b6cb55aabe1cd0983f41108282d2a8de38e1fda6
Umer Qureshi <Umerqis21@gmail.com>
2026-02-11 20:01:20 -0500

    font

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,9 @@
-
 function App() {
-
-  return <div className="bg-red-400">
-hello guys
-  </div>;
+  return (
+    <div className="bg-red-400 text-[100px] fred-light">
+      Welcome to Karahi Hub!
+    </div>
+  );
 }
 
 export default App;
